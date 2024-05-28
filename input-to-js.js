0fc10b771be55f4bfc9faf8769e194bcef916aed
NatapasL <natapas.lue@gmail.com>
2024-05-28 11:47:32 +0700

    Change regex on split input

--- input-to-js.js
+++ input-to-js.js
@@ -1,7 +1,8 @@
 const fs = require('fs')
 
 const multilineInput = fs.readFileSync('./input', { encoding: 'utf-8' })
-const parsedLine = multilineInput.split('\r\n').map((line) => `\n\t'${line}'`)
+console.log(multilineInput)
+const parsedLine = multilineInput.split('\n').map((line) => `\n\t'${line}'`)
 
 const outputText = `const input = [${parsedLine}\n]
 
